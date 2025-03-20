1cbac455f0464fe7da7daf45ba37d4ef9346cf67
Ashab <asxabal7@gmail.com>
2025-03-20 03:03:39 +0300

    solved the task-20

---
+++ task-20.py
@@ -0,0 +1,16 @@
+#создать массив из 10 целых случайных чисел
+
+array = [
+    id("ab"),
+    id("ba"),
+    id("cd"),
+    id("dc"),
+    id("ef"),
+    id("fe"),
+    id("gh"),
+    id("hg"),
+    id("ij"),
+    id("ji"),
+]
+
+print(array)
