f9d9a55a425ba5bc1f8918996764005aabbd2ca6
Ashab <asxabal7@gmail.com>
2025-03-21 19:53:34 +0300

    fix/task 18

--- task_18.py
+++ task_18.py
@@ -1,14 +1,14 @@
 #найти максимальный элемент в массиве целых чисел
 
 
-array = [2, 4, 6, 8, 10, 110] 
-index = 0
-max_number = 0
+array = [-2, -4, -6, -8, -10, -110] 
+i = 0
+max_number = array[i]
 
-while index <= len(array) - 1:
-    if array[index] > max_number:
-        max_number = array[index]
-    index += 1
+while i <= len(array) - 1:
+    if array[i] > max_number:
+        max_number = array[i]
+    i += 1
 
-print(max_number)
+print(max_number) # -2 а если все числа будут положительными, то выведет 110
 
