86ec8a3b920ad82b397b82cf52eefce894b281cf
Ashab <asxabal7@gmail.com>
2025-03-21 17:32:16 +0300

    fix/task-20

--- task-20.py
+++ task-20.py
@@ -1,16 +1,11 @@
 #создать массив из 10 целых случайных чисел
 
-array = [
-    id("ab"),
-    id("ba"),
-    id("cd"),
-    id("dc"),
-    id("ef"),
-    id("fe"),
-    id("gh"),
-    id("hg"),
-    id("ij"),
-    id("ji"),
-]
+import random
 
+array = []
+max_size = 10
+
+while len(array) != max_size:
+    array = array + [int(random.random() * 100000)]
+    
 print(array)
